49a6882961cca15c3e5f7093cdf11eea67b393af
Balint Pataki <patakibalintka@gmail.com>
2021-08-23 10:13:16 +0200

    creating templates

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from ckeditor.fields import RichTextField
+from django.urls import reverse
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -17,6 +18,12 @@ class Post(models.Model):
     status = models.CharField(max_length=10, 
             choices=STATUS_CHOICES,
             default='draft')
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                args=[self.publish.year,
+                    self.publish.month,
+                    self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
